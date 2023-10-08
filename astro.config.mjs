dee578c8d6621b5bee2a7a645c91a67fb1b941e5
Md Azharuddin <mdazhar.1038@gmail.com>
2023-10-08 17:16:06 +0530

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://azhar1038.github.com",
+  site: "https://azhar1038.github.io",
   base: "/astro-demo-learn",
 });
